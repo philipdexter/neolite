cd5ec10b863fad8b175d514354d1f80a2ab189e7
Philip Dexter <philip.dexter@gmail.com>
2020-02-03 11:39:04 +0200

    Add tests for rels

--- lib/storage/storage_test.go
+++ lib/storage/storage_test.go
@@ -31,5 +31,20 @@ func TestFromString(t *testing.T) {
 	assert.Equal(Props()[2].val, "deluxe")
 	assert.Equal(Props()[2].next, -1)
 
-	// TODO test rels
+	Reset()
+	err = FromString("NODES\n0 person\n1 car\n2 person\nPROPS\n0 name bob\n0 age 19\n1 type deluxe\nRELS\n0 1 owns\n2 1 borrows\n")
+	assert.Nil(err)
+	assert.Len(Nodes(), 3)
+	assert.Len(Props(), 3)
+	assert.Len(Rels(), 2)
+	assert.Equal(Rels()[0].Typ, "owns")
+	assert.Equal(Rels()[0].from, 0)
+	assert.Equal(Rels()[0].to, 1)
+	assert.Equal(Rels()[0].fromNext, -1)
+	assert.Equal(Rels()[0].toNext, -1)
+	assert.Equal(Rels()[1].Typ, "borrows")
+	assert.Equal(Rels()[1].from, 2)
+	assert.Equal(Rels()[1].to, 1)
+	assert.Equal(Rels()[1].fromNext, -1)
+	assert.Equal(Rels()[1].toNext, 0)
 }
